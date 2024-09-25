e1e3c711edcd121625529789fce932ab754dabab
Rhea <rheagok456@gmail.com>
2024-09-24 20:03:36 -0400

    gh-pages added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Personal-Website/', // Update this to match your GitHub repository name
   plugins: [react()],
-})
+});
